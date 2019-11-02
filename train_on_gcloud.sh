3e093e04a8fa2ecb70d79bad6150da5546bca4f1
Frank Kloster <frank.kloster@gmail.com>
2019-11-01 22:21:30 -0700

    Changed config to allow a higher memory GPU.

--- train_on_gcloud.sh
+++ train_on_gcloud.sh
@@ -1,2 +1,10 @@
 now=$(date +"%Y%m%d_%H%M%S")
-gcloud ai-platform jobs submit training RNSA_resnet50_$now --stream-logs --runtime-version 1.14 --job-dir gs://gcc-models --package-path trainer --module-name trainer.task --region us-central1 --python-version 3.5 --config config.yaml
+gcloud ai-platform jobs submit training rsna_inception_$now \
+    --stream-logs \
+    --runtime-version 1.14 \
+    --job-dir gs://gcc-models \
+    --package-path trainer \
+    --module-name trainer.task \
+    --region us-central1 \
+    --python-version 3.5 \
+    --config config.yaml
